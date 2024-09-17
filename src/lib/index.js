ef0613c56156484646fc8f6388fedc03cd4f17c7
Daniel Ho <hoduchai652007@gmail.com>
2024-09-17 12:09:30 +0700

    done

---
+++ src/lib/index.js
@@ -0,0 +1,2 @@
+// Reexport your entry components here
+export { default as QuillEditor } from "./QuillEditor.svelte"
---
+++ src/lib/quill.js
@@ -0,0 +1,51 @@
+// import Quill from "quill"
+export let Quill
+import { Delta } from "quill/core.js"
+
+export function quill(node, data, options) {
+    const { plainclipboard = false, ...remainedOptions } = options
+
+    const toolbar = [
+        [{ header: 1 }, { header: 2 }],
+        ["bold", "italic", "underline"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["link", "image", "video"],
+        ["code-block", "clean"],
+    ]
+
+    const q = new Quill(node, {
+        modules: {
+            toolbar,
+            resize: {
+                locale: {},
+            },
+        },
+        placeholder: "Type here...",
+        ...remainedOptions,
+    })
+
+    if (plainclipboard === true) {
+        q.clipboard.addMatcher(Node.ELEMENT_NODE, (n) => {
+            const text = n.textContent
+            return new Delta().insert(text)
+        })
+    }
+
+    const onTextChange = () => {
+        const customEvent = new CustomEvent("text-change", {
+            detail: {
+                html: q.getSemanticHTML(),
+                text: q.getText(),
+            },
+        })
+        node.dispatchEvent(customEvent)
+    }
+
+    q.on("text-change", onTextChange)
+
+    q.clipboard.dangerouslyPasteHTML(data, "api")
+
+    return () => {
+        q.off("text-change", onTextChange)
+    }
+}
